5ba480e50733e6eb0fd85f87cc638eb11af195d5
Vahtee Boo <vahteeboo@gmail.com>
2022-01-12 11:30:30 +0200

    Initial Commit

---
+++ P8-05-11/P8_05_11App.swift
@@ -0,0 +1,17 @@
+//
+//  P8_05_11App.swift
+//  P8-05-11
+//
+//  Created by Vahtee Boo on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P8_05_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
